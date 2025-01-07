a90d36ba5c357cb70f31b546f9970fcf1b674f2a
madforlife <madforlifee@gmail.com>
2025-01-07 16:21:23 +0200

    Include ModelMapper in the project
    Updated - build.gradle
    Add - ModelMapperConfig, ModelMapperUtil

---
+++ src/main/java/student/informatics/medicalrecord/utility/ModelMapperUtil.java
@@ -0,0 +1,27 @@
+package student.informatics.medicalrecord.utility;
+
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+
+@Component
+public class ModelMapperUtil {
+
+    private final ModelMapper modelMapper;
+
+    public <S, T> List<T> mapList(List<S> source, Class<T> targetClass) {
+        return source
+                .stream()
+                .map(element -> modelMapper.map(element, targetClass))
+                .collect(Collectors.toList());
+    }
+
+    public <S, T> T map(S source, Class<T> targetClass) {
+        return modelMapper.map(source, targetClass);
+    }
+}
